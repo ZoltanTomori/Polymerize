48ce5df438064e51427319bcb137eeebace6b944
Zoltan Tomori <tomori@outlook.sk>
2020-01-19 12:08:08 +0100

    Ver 1.0 (s rozsirenym lemom stlpikov

--- viscoStr.py
+++ viscoStr.py
@@ -2,34 +2,33 @@ import microstructure as mStr
 from numpy import linspace, pi, sin, cos, concatenate, zeros_like, zeros
 from copy import deepcopy
 
-# yfrom tpp import *
-# definujeme parametre na zaciatok
-speed = 50
+# slab
+slabSpeed = 50
+xyres = 0.25
+slabSz = [6, 5, 8] 
+slabExtSz = [8, 5, 1.3]
+slabSzDifX = slabExtSz[0] - slabSz[0]
+
+# line
 linespeed = 80
 lineLength = 10
+linePoints = 20
+lineElev = slabSz[2] + slabExtSz[2]/2
 
+# sphere
 sphereR = 1.0
 sphereSpeed = 20
 
-xyres = 0.25
-slabXY = 5 
-slabZ = 10
-lineFromGround = 7
-
 # _______________________________________________________________________
 def sinusLine():
-    x = linspace(0, 2 * lineLength, num=20)
+    x = linspace(0, 2 * lineLength, linePoints)
     y = zeros_like(x)
-    z = zeros_like(x)+9
-    for i in range(len(x)):
-        y[i] = 0
-        z[i] = 9
-
-    xCos = linspace(0, 2 * pi, num=10)
+    z = zeros_like(x) + lineElev
+   
+    xCos = linspace(0, 2 * pi, linePoints/2)
     yCos = cos(xCos) - 1
     for i in range(len(xCos)):       
-        y[i + lineLength] = yCos[i] * 5
-        z[i + lineLength] = 9
+        z[i + lineLength] = lineElev + 2 * yCos[i]
 
     sinLine = zeros((len(x), 5))
     sinLine[:, 0] = x
@@ -41,25 +40,29 @@ def sinusLine():
     return sinLine
 
 
-viscoStr = mStr.slabStr([slabXY, slabXY, slabZ], [0, 1, 2], [xyres, 0.5], speed)
-viscoStr.shift([-slabXY, -slabXY/2, 0])
-slab2 = deepcopy(viscoStr)
-slab2.shift([(lineLength)*2 + slabXY, 0, 0])
-viscoStr.addStr(slab2)
+slab1 = mStr.slabStr(slabSz, [0, 1, 2], [xyres, 0.5], slabSpeed)
+slab1.shift([-slabSz[0], -slabSz[1]/2, 0])
+viscoStruct = slab1
+
+slabExt = mStr.slabStr(slabExtSz, [0, 1, 2], [xyres, 0.5], slabSpeed)
+slabExt.shift([-(slabSz[0] + slabSzDifX/2), -slabExtSz[1]/2, slabSz[2]])
+viscoStruct.addStr(slabExt)
+
+slab2 = deepcopy(viscoStruct)
+slab2.shift([(lineLength)*2 + slabSz[0], 0, 0])
+viscoStruct.addStr(slab2)
 
 sinLine = sinusLine()
 line = mStr.MicroStr(sinLine)
-#line.shift([2.0, 0, 0])
-viscoStr.addStr(line)
+viscoStruct.addStr(line)
 
 # nakoniec sa vytvori a prida do struktury sfera, parametre: prve tri su
 # suradnice stredu, polomer, rychlost, xyres a dalsie dva parametre
 # neviem co su zac,nema to vysvetlene ani v dokumentacii, ale shell spacing
 # je vzdialenost medzi jednotlivymi vrstavami gule
-sph = mStr.sphereStr(lineLength, 0, 9, sphereR,
+sph = mStr.sphereStr(lineLength, 0, lineElev, sphereR,
                      sphereSpeed, xyres, 1.0, 1, shellspacing=0.5)
-viscoStr.addStr(sph)
-viscoStr.plot(1, markerscalef=0.1)
-c = 0
+viscoStruct.addStr(sph)
+viscoStruct.plot(1, markerscalef=0.1)
 
 
