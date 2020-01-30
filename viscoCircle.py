84951b9eadb159a17b3bfb5c9fc7ecaa48e7fa47
Zoltan Tomori <tomori@outlook.sk>
2020-01-30 15:01:44 +0100

    4 uchytky, 2 luce rovne

--- viscoCircle.py
+++ viscoCircle.py
@@ -2,10 +2,14 @@ import microstructure as mStr
 from numpy import linspace, pi, sin, cos, concatenate, zeros_like, zeros
 from copy import deepcopy
 
+# _______________________________________________________________________
+
 # slab
-slabSpeed = 50
-xyres = 0.25
-slabSz = [1, 1, 3]
+numSlabs = 4            # pocet stlpikov podopierajucich gulovite uchytky
+numRays = 2             # pocet lucov
+slabSpeed = 50          # rychlost polymerizovania stlpika
+xyres = 0.25            # vzdialenost medzi ciarami pri budovani stlpika
+slabSz = [1, 1, 3]      # rozmer stlpika [x,y,z]
 
 # circle
 circSpeed = 80          # rychlost polymerizacie kruhu
@@ -17,7 +21,7 @@ circDist = 0.1          # vzdialenost kruznic pri polymerizovani kruhu
 # sphere
 sphereR = 1.0           # polomer gulicky na stlpikoch
 sphereRayR = 0.5        # polomer gulicky "zdureniny" z ktrej ide luc z kruhu
-sphereSpeed = 20
+sphereSpeed = 20        # rychlost polymerizovania gulicky
 
 # line
 lineLength = 18         # dlzka ramena luca (ma byt > circR)
@@ -37,12 +41,12 @@ def circle(radius, elev):
     return arr
 
 # rozdeli 0-360 stupnov na 3 casti (4 okrajove body)
-verts = linspace(0, 2*pi, 4)
+verts = linspace(pi/4, 9*pi/4, numSlabs+1)
 slabPosX = cos(verts) * circR
 slabPosY = sin(verts) * circR
 slab0 = mStr.slabStr([0.2, 0.2, 0.2], [0, 1, 2], [xyres, 0.2], slabSpeed)
 viscoStruct = slab0
-for i in range(3):
+for i in range(numSlabs):
     #stlpik pod kruhom ukonceny gulickou
     slab1 = mStr.slabStr([slabSz[0], slabSz[1], slabSz[2] + sphereR/3],[0, 1, 2], [xyres, 0.2], slabSpeed)
     slab1.shift([slabPosX[i]-slabSz[0]/2, slabPosY[i]-slabSz[1]/2, 0])
@@ -58,12 +62,12 @@ for i in range(-numCircles, numCircles):
         viscoStruct.addStr(circ)
 
 # rozdeli 45-405 stupnov na 3 casti (4 okrajove body)
-vertsRay = linspace(pi/3, 2*pi+pi/3, 4)
+vertsRay = linspace(0, 2*pi, numRays+1)
 slabRayPosX = cos(vertsRay) * circR
 slabRayPosY = sin(vertsRay) * circR
 
  # stlpiky pod zaciatkom a koncom luca ukoncene gulickou
-for i in range(3):
+for i in range(numRays):
     sphRay1 = mStr.sphereStr(slabRayPosX[i], slabRayPosY[i], slabSz[2]+sphereR, sphereRayR, sphereSpeed, xyres, 1.0, 1, shellspacing=0.5)
     viscoStruct.addStr(sphRay1)
 
@@ -74,8 +78,7 @@ for i in range(3):
     viscoStruct.addStr(sphRay2)
    
 # luce az nakoniec lebo su tenke
-for i in range(3):
-
+for i in range(numRays):
     lineArr = zeros((2, 5))
     lineArr[:, 0] = [slabRayPosX[i], lineLengthCoeff * slabRayPosX[i]]
     lineArr[:, 1] = [slabRayPosY[i], lineLengthCoeff * slabRayPosY[i]]
@@ -84,4 +87,8 @@ for i in range(3):
     lineArr[-1, 3:5] = [0, lineSpeed]
     viscoStruct.addStr(mStr.MicroStr(lineArr))
 
+# gulicka do stredu luca
+for i in range(numRays):
+    sphRay2 = mStr.sphereStr((lineLengthCoeff+1)*slabRayPosX[i]/2, (lineLengthCoeff+1)*slabRayPosY[i]/2, slabSz[2]+sphereR, sphereRayR, sphereSpeed, xyres, 1.0, 1, shellspacing=0.5)
+    viscoStruct.addStr(sphRay2)
 viscoStruct.plot(1, markerscalef=0.1)
